d55d440550f9f6e6be39e58dda9e52bbe7453116
Robin Suter <suter.robin@gmail.com>
2017-03-24 14:37:51 +0100

    initial OR Mapper setup

---
+++ examibur/src/main/java/ch/examibur/domain/User.java
@@ -0,0 +1,62 @@
+package ch.examibur.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_t")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userId;
+	private String firstName;
+	private String lastName;
+	
+	
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (userId ^ (userId >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ examibur/src/main/java/ch/examibur/integration/utils/EntityManagerHelper.java
@@ -0,0 +1,18 @@
+package ch.examibur.integration.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public enum EntityManagerHelper {
+	INSTANCE;
+	private EntityManagerFactory factory;
+	
+	private EntityManagerHelper() {
+		factory = Persistence.createEntityManagerFactory("examibur");
+	}
+	
+	public EntityManager createEntityManager() {
+		return factory.createEntityManager();
+	}
+}
