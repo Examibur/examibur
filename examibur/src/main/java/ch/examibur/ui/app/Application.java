da9b1eb3f9e1764b56a65207f835cc498189f25b
pacs01 <pacs01dev@gmail.com>
2017-03-26 22:44:58 +0200

    add error handling to routing

--- examibur/src/main/java/ch/examibur/ui/app/Application.java
+++ examibur/src/main/java/ch/examibur/ui/app/Application.java
@@ -5,6 +5,7 @@ import static spark.Spark.before;
 import static spark.Spark.staticFiles;
 
 import ch.examibur.ui.app.controller.DashboardController;
+import ch.examibur.ui.app.controller.ExceptionController;
 import ch.examibur.ui.app.filter.Filters;
 import spark.servlet.SparkApplication;
 
@@ -16,10 +17,12 @@ public class Application implements SparkApplication {
     staticFiles.location("/public");
 
     DashboardController dashboardController = new DashboardController();
+    ExceptionController exceptionController = new ExceptionController();
 
     before("*", Filters.addTrailingSlashes);
 
     dashboardController.route();
+    exceptionController.route();
 
     after("*", Filters.addGzipHeader);
 
---
+++ examibur/src/main/java/ch/examibur/ui/app/controller/ExceptionController.java
@@ -0,0 +1,54 @@
+package ch.examibur.ui.app.controller;
+
+import static spark.Spark.exception;
+import static spark.Spark.get;
+
+import ch.examibur.ui.app.util.TemplateRenderer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jetty.http.HttpStatus;
+
+import spark.Request;
+import spark.Response;
+
+public class ExceptionController extends Controller {
+
+  /**
+   * Handles Exception thrown in a controller.
+   * 
+   * @param exception
+   *          the exception thrown in a controller
+   * @param request
+   *          the HTTP request
+   * @param response
+   *          the HTTP response
+   */
+  public void handleException(Exception exception, Request request, Response response) {
+    response.status(HttpStatus.INTERNAL_SERVER_ERROR_500);
+    response.body("Error handling or redirect...");
+  }
+
+  /**
+   * Returns error page with HTTP 404 status.
+   * 
+   * @param request
+   *          the HTTP request
+   * @param response
+   *          the HTTP response
+   */
+  public String notFound(Request request, Response response) {
+    response.status(HttpStatus.NOT_FOUND_404);
+    Map<String, Object> model = new HashMap<>();
+    model.put("title", controllerName);
+    return new TemplateRenderer().render(model, "404.ftl");
+  }
+
+  @Override
+  public void route() {
+    get("*", this::notFound);
+    exception(Exception.class, this::handleException);
+  }
+
+}
