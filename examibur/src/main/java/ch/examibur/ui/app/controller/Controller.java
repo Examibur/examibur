6e3e59a25a231c9fac98a465d9c58ec6184790bc
pacs01 <pacs01dev@gmail.com>
2017-03-29 23:33:33 +0200

    add model field and access methods to the base controller

--- examibur/src/main/java/ch/examibur/ui/app/controller/Controller.java
+++ examibur/src/main/java/ch/examibur/ui/app/controller/Controller.java
@@ -1,9 +1,13 @@
 package ch.examibur.ui.app.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Controller {
 
   private final Controller preController;
   protected final String relativePath;
+  private Map<String, Object> model = new HashMap<>();
 
   public Controller(Controller preController, String relativePath) {
     this.preController = preController;
@@ -22,6 +26,30 @@ public abstract class Controller {
       return preController.getAbsolutePath() + relativePath;
     }
     return relativePath;
+  }
+
+  /**
+   * Returns a merged model from all controllers within the callers path. Values
+   * from lower controller models with the same key will be overwritten by the
+   * new values.
+   * 
+   * @return the merged model
+   */
+  public Map<String, Object> getFullPathModel() {
+    Map<String, Object> result = new HashMap<>();
+    if (preController != null) {
+      result.putAll(preController.getModel());
+    }
+    result.putAll(model);
+    return result;
+  }
+
+  public Map<String, Object> getModel() {
+    return model;
+  }
+
+  public void setModel(Map<String, Object> model) {
+    this.model = model;
   }
 
 }
