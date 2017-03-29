0598641c90cb8d722b2ec3d857b359e9723a1a34
Raphael Zimmermann <mail@raphael.li>
2017-03-29 20:13:46 +0200

    Only backup specified volumes

--- projekteserver/backup/backup.sh
+++ projekteserver/backup/backup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -e
 
-VOLUMES_DIR=/var/lib/docker/volumes/
+VOLUMES_DIR=/var/lib/docker/volumes
 SERVICE=examibur-dev.service
 DATE=$(date +"%Y_%m_%d-%H-%M-%S")
-DESTINATION=/opt/backups/
+DESTINATION=/opt/backups
+
+VOLUMES_PREFIX='projekteserver_'
+VOLUMES=('nginx_dehydrated' 'db_sonarqube' 'db_examibur' 'sonarqube_data' 'sonarqube_extensions')
 
 mkdir -p ${DESTINATION}
-systemctl stop $SERVICE 
-tar cvzf "examibur-dev-${DATE}.tar.gz" "$VOLUMES_DIR"
+echo "Stopping service $SERVICE"
+systemctl stop $SERVICE
+
+for VOLUME in "${VOLUMES[@]}"
+do
+	echo "Backing up $VOLUME..."
+	tar cvzf "${DESTINATION}/examibur-dev-${DATE}-${VOLUME}.tar.gz" "${VOLUMES_DIR}/${VOLUMES_PREFIX}${VOLUME}"
+done
+
+echo "Starting service $SERVICE"
 systemctl start $SERVICE
+
