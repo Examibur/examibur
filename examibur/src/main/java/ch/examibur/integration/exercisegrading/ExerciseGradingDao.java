4cfb0ebd38798a7ae7c0d17882ffa53cd0b72fde
Robin Suter <suter.robin@gmail.com>
2017-04-13 09:31:21 +0200

    add javadoc

--- examibur/src/main/java/ch/examibur/integration/exercisegrading/ExerciseGradingDao.java
+++ examibur/src/main/java/ch/examibur/integration/exercisegrading/ExerciseGradingDao.java
@@ -4,5 +4,12 @@ import ch.examibur.domain.ExamState;
 import ch.examibur.domain.ExerciseGrading;
 
 public interface ExerciseGradingDao {
+  /**
+   * @param exerciseSolutionId
+   * @param state
+   *        the ExamState the ExerciseSolution was created in, e.g. REVIEW or CORRECTION.
+   * @return
+   *        the ExerciseGrading if it was found, null otherwise.
+   */
   ExerciseGrading getGradingCreatedInState(long exerciseSolutionId, ExamState state);
 }
