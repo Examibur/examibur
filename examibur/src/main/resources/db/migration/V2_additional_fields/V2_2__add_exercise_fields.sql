599652b9c6ad8138cc18c90f34f39a99c8915d5e
Robin Suter <suter.robin@gmail.com>
2017-04-17 14:13:20 +0200

    add migration sql for exercise schema changes

---
+++ examibur/src/main/resources/db/migration/V2_additional_fields/V2_2__add_exercise_fields.sql
@@ -0,0 +1,42 @@
+ALTER TABLE exercise_t
+    ADD title character varying(255),
+    ADD orderinexam integer NOT NULL DEFAULT 0;
+    
+-- update demo data
+UPDATE exercise_t SET title = 'AES-CBC Disk Encryption' WHERE exerciseid = 1;
+UPDATE exercise_t SET title = 'XTS-AES Speicherplatz Ausnutzung' WHERE exerciseid = 2;
+UPDATE exercise_t SET title = '' WHERE exerciseid = 3;
+UPDATE exercise_t SET title = 'Verordnung gegen Verfassung' WHERE exerciseid = 4;
+UPDATE exercise_t SET title = 'Datenbearbeitung' WHERE exerciseid = 5;
+UPDATE exercise_t SET title = 'Aufsichtsbehörde Datenschutz' WHERE exerciseid = 6;
+UPDATE exercise_t SET title = 'Vorteile Datenbank-Systeme' WHERE exerciseid = 7;
+UPDATE exercise_t SET title = 'Wichtige Funktionen DBMS' WHERE exerciseid = 8;
+UPDATE exercise_t SET title = 'COALESCE-Funktion' WHERE exerciseid = 9;
+UPDATE exercise_t SET title = 'Verordnung gegen Verfassung' WHERE exerciseid = 10;
+UPDATE exercise_t SET title = 'Datenbearbeitung' WHERE exerciseid = 11;
+UPDATE exercise_t SET title = 'Zuständige Aufsichtsbehörde' WHERE exerciseid = 12;
+UPDATE exercise_t SET title = 'Vorteile Datenbank-Systeme' WHERE exerciseid = 13;
+UPDATE exercise_t SET title = 'Wichtige Funktionen DBMS' WHERE exerciseid = 14;
+UPDATE exercise_t SET title = 'COALESCE-Funktion' WHERE exerciseid = 15;
+UPDATE exercise_t SET title = 'AES-CBC Disk Encryption  ' WHERE exerciseid = 16;
+UPDATE exercise_t SET title = 'XTS-AES Speicherplatz Ausnutzung' WHERE exerciseid = 17;
+UPDATE exercise_t SET title = '' WHERE exerciseid = 18;
+
+UPDATE exercise_t SET orderinexam = 1 WHERE exerciseid = 1;
+UPDATE exercise_t SET orderinexam = 2 WHERE exerciseid = 2;
+UPDATE exercise_t SET orderinexam = 3 WHERE exerciseid = 3;
+UPDATE exercise_t SET orderinexam = 1 WHERE exerciseid = 4;
+UPDATE exercise_t SET orderinexam = 2 WHERE exerciseid = 5;
+UPDATE exercise_t SET orderinexam = 3 WHERE exerciseid = 6;
+UPDATE exercise_t SET orderinexam = 1 WHERE exerciseid = 7;
+UPDATE exercise_t SET orderinexam = 2 WHERE exerciseid = 8;
+UPDATE exercise_t SET orderinexam = 3 WHERE exerciseid = 9;
+UPDATE exercise_t SET orderinexam = 1 WHERE exerciseid = 10;
+UPDATE exercise_t SET orderinexam = 2 WHERE exerciseid = 11;
+UPDATE exercise_t SET orderinexam = 3 WHERE exerciseid = 12;
+UPDATE exercise_t SET orderinexam = 1 WHERE exerciseid = 13;
+UPDATE exercise_t SET orderinexam = 2 WHERE exerciseid = 14;
+UPDATE exercise_t SET orderinexam = 3 WHERE exerciseid = 15;
+UPDATE exercise_t SET orderinexam = 1 WHERE exerciseid = 16;
+UPDATE exercise_t SET orderinexam = 2 WHERE exerciseid = 17;
+UPDATE exercise_t SET orderinexam = 3 WHERE exerciseid = 18;
