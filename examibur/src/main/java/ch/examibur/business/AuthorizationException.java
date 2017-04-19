0496b9124e23e7ddbeb8c2049fd75bc83bf5faf1
Robin Suter <suter.robin@gmail.com>
2017-04-19 14:55:33 +0200

    add universal exceptions on the business layer

---
+++ examibur/src/main/java/ch/examibur/business/AuthorizationException.java
@@ -0,0 +1,17 @@
+package ch.examibur.business;
+
+public class AuthorizationException extends Exception {
+
+  private static final long serialVersionUID = 4135500762348965806L;
+
+  public AuthorizationException(String message, Throwable cause) {
+    super(message, cause);
+    // TODO Auto-generated constructor stub
+  }
+
+  public AuthorizationException(String message) {
+    super(message);
+    // TODO Auto-generated constructor stub
+  }
+
+}
