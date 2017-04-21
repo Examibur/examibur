23443004aa88ca455a23f6c14fa627b20599e903
Robin Suter <suter.robin@gmail.com>
2017-04-21 21:40:59 +0200

    fix typo

--- docker/testing/ci_runtests.sh
+++ docker/testing/ci_runtests.sh
@@ -3,8 +3,8 @@
 curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-d/usr/local/bin/docker-compose up --build -d
-
+/usr/local/bin/docker-compose up --build -d
+pwd
 ./run_integration.sh
 
 /usr/local/bin/docker-compose stop
