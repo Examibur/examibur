0741955c68247f8cbb566cbba996dd3c8599c635
Robin Suter <suter.robin@gmail.com>
2017-04-21 22:21:34 +0200

    add examibur build to tomcat-dev

--- docker/testing/ci_runtests.sh
+++ docker/testing/ci_runtests.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-./docker-compose up --build -d
+/usr/local/bin/docker-compose up --build -d
 pwd
-/bin/sh run_integration.sh
+ls -l
+./run_integration.sh
 
-./docker-compose stop
+/usr/local/bin/docker-compose stop
 
