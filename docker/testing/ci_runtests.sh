aa00df98404cf855829979ea38cb2763bb314421
Robin Suter <suter.robin@gmail.com>
2017-04-24 13:08:29 +0200

    use docker-compose image for ci integration test

--- docker/testing/ci_runtests.sh
+++ docker/testing/ci_runtests.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 /usr/local/bin/docker-compose up --build -d
-pwd
-ls -l
-./run_integration.sh
+
+/bin/bash ./run_integration.sh
 
 /usr/local/bin/docker-compose stop
 
