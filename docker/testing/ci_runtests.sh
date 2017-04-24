f33f8471994f9482557db468a489398599d85487
Robin Suter <suter.robin@gmail.com>
2017-04-24 13:15:56 +0200

    remove docker-compose installation

--- docker/testing/ci_runtests.sh
+++ docker/testing/ci_runtests.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+docker-compose up --build -d
 
-/usr/local/bin/docker-compose up --build -d
+/bin/bash /builds/engineering-projekt/examibur/run-integration.sh
 
-/bin/bash ./run_integration.sh
-
-/usr/local/bin/docker-compose stop
+docker-compose stop
 
