30a333545a74bd88f05ab0c793fea4554e5474ea
Robin Suter <suter.robin@gmail.com>
2017-04-26 18:31:23 +0200

    add integration tests for getExercises()

--- examibur/src/integrationtest/java/ch/examibur/business/exercise/ExerciseServiceImplTest.java
+++ examibur/src/integrationtest/java/ch/examibur/business/exercise/ExerciseServiceImplTest.java
@@ -5,6 +5,8 @@ import static ch.examibur.business.IntegrationTestUtil.INJECTOR;
 import ch.examibur.business.DatabaseResource;
 import ch.examibur.business.exception.ExamiburException;
 import ch.examibur.business.exception.NotFoundException;
+import ch.examibur.domain.Exercise;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -30,6 +32,36 @@ public class ExerciseServiceImplTest {
   @Test(expected = NotFoundException.class)
   public void testGetMaxPointsWithNonexistentExamId() throws ExamiburException {
     exerciseService.getMaxPoints(0);
+  }
+
+  @Test
+  public void testGetExercises() {
+    List<Exercise> exList = exerciseService.getExercises(3L);
+    Assert.assertEquals(3, exList.size());
+    for (Exercise ex : exList) {
+      Assert.assertNotNull(ex);
+    }
+  }
+
+  @Test
+  public void testGetExercisesForExamWithoutExercises() {
+    List<Exercise> exList = exerciseService.getExercises(1L);
+    Assert.assertNotNull(exList);
+    Assert.assertEquals(0, exList.size());
+  }
+
+  @Test
+  public void testGetExercisesForNonExistingExamId() {
+    List<Exercise> exList = exerciseService.getExercises(0L);
+    Assert.assertNotNull(exList);
+    Assert.assertEquals(0, exList.size());
+  }
+
+  @Test
+  public void testGetExercisesForExamWithNegativeId() {
+    List<Exercise> exList = exerciseService.getExercises(-42L);
+    Assert.assertNotNull(exList);
+    Assert.assertEquals(0, exList.size());
   }
 
 }
