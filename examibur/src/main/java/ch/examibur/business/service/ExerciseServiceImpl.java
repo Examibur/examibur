21af88f46d113eed0c6c69f7cac049ea90dc8f2e
Jonas Matter <jonas.matter@bluewin.ch>
2017-05-14 19:43:37 +0200

    validate exercise id in getExercise

--- examibur/src/main/java/ch/examibur/business/service/ExerciseServiceImpl.java
+++ examibur/src/main/java/ch/examibur/business/service/ExerciseServiceImpl.java
@@ -1,12 +1,19 @@
 package ch.examibur.business.service;
 
+import ch.examibur.business.util.ValidationHelper;
 import ch.examibur.domain.Exercise;
 import ch.examibur.integration.exercise.ExerciseDao;
 import ch.examibur.service.ExerciseService;
+import ch.examibur.service.exception.ExamiburException;
+import ch.examibur.service.exception.InvalidParameterException;
 import com.google.inject.Inject;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExerciseServiceImpl implements ExerciseService {
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExerciseServiceImpl.class);
 
   private final ExerciseDao exerciseDao;
 
@@ -21,7 +28,8 @@ public class ExerciseServiceImpl implements ExerciseService {
   }
 
   @Override
-  public Exercise getExercise(long exerciseId) {
+  public Exercise getExercise(long exerciseId) throws ExamiburException {
+    ValidationHelper.checkForNegativeId(exerciseId, LOGGER);
     return exerciseDao.getExercise(exerciseId);
   }
 
--- examibur/src/main/java/ch/examibur/service/ExerciseService.java
+++ examibur/src/main/java/ch/examibur/service/ExerciseService.java
@@ -1,6 +1,11 @@
 package ch.examibur.service;
 
 import ch.examibur.domain.Exercise;
+import ch.examibur.service.exception.AuthorizationException;
+import ch.examibur.service.exception.CommunicationException;
+import ch.examibur.service.exception.ExamiburException;
+import ch.examibur.service.exception.InvalidParameterException;
+import ch.examibur.service.exception.NotFoundException;
 import java.util.List;
 
 public interface ExerciseService {
@@ -11,12 +16,17 @@ public interface ExerciseService {
    * @return a list of all exercises for the given exam.
    */
   List<Exercise> getExercises(long examId);
-  
+
   /**
    * @param exerciseId
    *          the id of the exercise
    * @return the exercise for the specified exercise id.
+   * @throws ExamiburException
+   *           throws {@link InvalidParameterException} if exerciseId < 0. throws
+   *           {@link NotFoundException} if the {@link Exercise} with the given id doesn't exist.
+   *           throws {@link AuthorizationException} if the user is not authorized. throws
+   *           {@link CommunicationException} if an exception during the communication occurs.
    */
-  Exercise getExercise(long exerciseId);
+  Exercise getExercise(long exerciseId) throws ExamiburException;
 
 }
