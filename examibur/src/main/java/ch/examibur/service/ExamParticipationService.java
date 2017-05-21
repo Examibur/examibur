2ee63824f038bf5daabb79281c5c5fbc7d4c4322
Jonas Matter <jonas.matter@bluewin.ch>
2017-05-21 12:19:09 +0200

    add participant information view

--- examibur/src/main/java/ch/examibur/service/ExamParticipationService.java
+++ examibur/src/main/java/ch/examibur/service/ExamParticipationService.java
@@ -15,8 +15,30 @@ public interface ExamParticipationService {
    * @param examId
    *          id of the exam. This exam must exist!
    * @return a list of all participants of the given exam.
+   * @throws ExamiburException
+   *           throws {@link InvalidParameterException} if examParticipationId < 0. throws
+   *           {@link NotFoundException} if the {@link ExamParticipation} with the given id was not
+   *           found. throws {@link AuthorizationException} if the user is not authorized to display
+   *           this ExamParticipation. throws {@link CommunicationException} if an exception during
+   *           the communication occurs.
    */
-  List<ExamParticipantOverview> getExamParticipantsOverview(long examId);
+  List<ExamParticipantOverview> getExamParticipantOverviewList(long examId)
+      throws ExamiburException;
+
+  /**
+   * @param examParticipationId
+   *          id of the exam participation
+   * @return an exam participant overview {@link ExamParticipantOverview} of the specific exam
+   *         participation.
+   * @throws ExamiburException
+   *           throws {@link InvalidParameterException} if examParticipationId < 0. throws
+   *           {@link NotFoundException} if the {@link ExamParticipation} with the given id was not
+   *           found. throws {@link AuthorizationException} if the user is not authorized to display
+   *           this ExamParticipation. throws {@link CommunicationException} if an exception during
+   *           the communication occurs.
+   */
+  ExamParticipantOverview getExamParticipantOverview(long examParticipationId)
+      throws ExamiburException;
 
   /**
    * @param examParticipationId
