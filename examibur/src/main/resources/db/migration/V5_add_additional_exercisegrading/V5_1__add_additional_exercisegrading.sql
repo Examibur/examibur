0df6ab000c3e71803c19c1bdc39fbaa9fbd8b319
Robin Suter <suter.robin@gmail.com>
2017-05-26 10:47:33 +0200

    fix inconsistent exercisesolution test data

--- examibur/src/main/resources/db/migration/V5_add_additional_exercisegrading/V5_1__add_additional_exercisegrading.sql
+++ examibur/src/main/resources/db/migration/V5_add_additional_exercisegrading/V5_1__add_additional_exercisegrading.sql
@@ -1,2 +1,4 @@
 INSERT INTO exercisegrading_t (createdinstate, creationdate, isfinalgrading, exercisegrading_authorid, exercisegrading_exercisesolutionid, points, comment, reasoning) values
 	('CORRECTION', '2017-01-29', TRUE, 5, 52, 2, 'Rechnungsfehler', '');
+
+UPDATE exercisesolution_t SET isdone = TRUE WHERE exercisesolutionid = 52;
